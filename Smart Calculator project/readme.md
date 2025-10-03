84601197db3dc6d47020d9f758c20d60a73b8b9f
Mehrab Hasan Sompod <mdmehrabhasansompod@gmail.com>
2025-10-03 09:20:34 +0600

    Smart calculator project directory uploaded

---
+++ Smart Calculator project/readme.md
@@ -0,0 +1,13 @@
+# Smart Calculator 🧮
+
+A simple and functional calculator built with vanilla HTML, CSS, and JavaScript. No frameworks, no libraries — just pure code.
+
+![Smart Calculator](/calculator.png)
+
+---
+
+## Tech Stack 🛠️
+
+- **HTML** – structure of the calculator  
+- **CSS** – styling and responsive design  
+- **JavaScript** – functionality for operations, clear, and delete
---
+++ Smart Calculator project/script.js
@@ -0,0 +1,68 @@
+let currentNum = '';
+let prevNum = '';
+let currentOperation='';
+
+const appendNum = (num) => {
+  currentNum += num;
+  document.getElementById('result').value = `${prevNum} ${currentOperation} ${currentNum}`;
+  scrollLeft()
+}
+
+const appendOperation = (operation) => {
+    if(currentNum === '') return;
+    if(!prevNum ===''){
+        calculate()
+    }
+    prevNum = currentNum;
+    currentOperation = operation;
+    currentNum = '';
+    document.getElementById('result').value = `${prevNum} ${currentOperation}`;
+    scrollLeft()
+}
+
+const calculate = () => {
+    if(prevNum === '' || currentNum === '' ) return;
+    let result;
+    let prev = parseFloat(prevNum);
+    let current = parseFloat(currentNum);
+    switch(currentOperation){
+        case '+': result = prev + current; 
+        break;
+        case '-': result = prev - current;
+        break;
+        case '*': result = prev * current;
+        break;
+        case '/': result = current === 0 ? "Undefined" : prev / current;
+        break;
+        case '%': result = prev % current;
+        break;
+        default: return;
+    }
+
+    currentNum = '';
+    prevNum = '';
+    currentOperation ='';
+    document.getElementById('result').value = result.toString();
+    scrollLeft()
+}
+
+const eraseDisplay = () => {
+   let displayVal = document.getElementById('result').value;
+   let newDisplayVal = displayVal.slice(0,-1);
+   document.getElementById('result').value = newDisplayVal;
+   scrollLeft()
+}
+
+
+const clearDisplay = () => {
+    currentNum = '';
+    prevNum ='';
+    currentOperation ='';
+    document.getElementById('result').value = '';
+    scrollLeft()
+}
+
+const scrollLeft = () => {
+    const resultBox = document.getElementById('result');
+    resultBox.scrollLeft = resultBox.scrollWidth; 
+}
